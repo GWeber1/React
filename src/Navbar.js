75b52135c9a142514bf8f0e8fb501996e0e4444a
GWeber1 <gabrielweber2001@gmail.com>
2021-08-13 14:56:01 -0300

    Inicio do projeto

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => (
+    <nav className =  "navbar">
+        <div className="navbar__div">
+            <a href="#" class="navbar--item">DevFinder</a>
+        </div>
+    </nav>
+)
+
+export default Navbar;
